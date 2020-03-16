ce430a59c6f91b1790499d8ec83d2a2f24f49ba1
Maya Estalilla <mayaestalilla@gmail.com>
2020-03-16 13:13:12 -0400

    Add "Show Games Played toggle"

--- src/PlayerList.js
+++ src/PlayerList.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Player from './Player';
 
-const PlayerList = props => {
-  const { players } = props;
+class PlayerList extends Component {
+  state = {
+    showPlayCount: true
+  };
 
-  return (
-    <div>
-      <h2>Player List</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Handle</th>
-            <th scope="col">
-              <a href="">Show Games Played</a>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {players.map((player, idx) => (
-            <Player
-              key={idx}
-              firstName={player.firstName}
-              lastName={player.lastName}
-              username={player.username}
-              playCount={player.playCount}
-              rowIndex={idx + 1}
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+  handleClick = e => {
+    e.preventDefault();
+
+    this.setState(prevState => ({
+      showPlayCount: !prevState.showPlayCount
+    }));
+  };
+
+  render() {
+    const { players } = this.props;
+    const { showPlayCount } = this.state;
+
+    return (
+      <div>
+        <h2>Player List</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Handle</th>
+              <th scope="col">
+                <a href="/" onClick={this.handleClick}>
+                  {showPlayCount ? 'Hide' : 'Show'} Games Played
+                </a>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {players.map((player, idx) => (
+              <Player
+                key={idx}
+                firstName={player.firstName}
+                lastName={player.lastName}
+                username={player.username}
+                playCount={showPlayCount ? player.playCount : '*'}
+                rowIndex={idx + 1}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default PlayerList;
